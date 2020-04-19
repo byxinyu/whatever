4a74562e3d6949d5583d323bed4170d73e786031
xinyu <169133431@qq.com>
2020-04-19 18:18:26 +0800

    开启debug

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yw5k#e9kp$1*)-%3ws7qv5jp87eb8*gvo)7=he3x!am1$u!-wv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.zhangxinyu.org']
 
@@ -124,4 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static/")
