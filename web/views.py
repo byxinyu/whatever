599e7d5901ded349130278cc6e526247363e601d
xinyu <1691333431@qq.com>
2020-06-10 20:48:13 +0800

    添加登录登出功能

--- web/views.py
+++ web/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post, Category, Tag
 from django.shortcuts import render, get_object_or_404
@@ -11,34 +10,19 @@ from django.views.generic import DetailView
 from pure_pagination.mixins import PaginationMixin
 from django.contrib import messages
 from django.db.models import Q
+from django.views.generic import ListView
 
-class IndexView(PaginationMixin,ListView):
+class IndexView(PaginationMixin, ListView):
     model = Post
     template_name = 'web/index.html'
     context_object_name = 'post_list'
     # 指定paginate_by 属性后开启分页功能，其值代表每一页包含多少篇文章
     paginate_by = 10
 
-# def detail(request, pk):
-#     post = get_object_or_404(Post, pk=pk)
 
-#     #阅读量 +1
-#     post.increase_views()
-
-#     md = markdown.Markdown(extensions=[
-#                                       'markdown.extensions.extra',
-#                                       'markdown.extensions.codehilite',
-#                                       # 'markdown.extensions.fenced_code',
-#                                       TocExtension(slugify=slugify),
-#     ])
-#     post.body = md.convert(post.body)
-#     m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
-#     post.toc = m.group(1) if m is not None else ''
-#     return render(request, 'web/detail.html', context={'post': post})
 
 
 class PostDetailView(DetailView):
-    #这些属性的含义和ListView 一样的
     model = Post
     template_name ='web/detail.html'
     context_object_name = 'post'
@@ -57,37 +41,15 @@ class PostDetailView(DetailView):
         #视图必须 返回一个HttpResponse对象
         return response
 
-    def get_object(self,queryset=None):
-        # 覆写 get_object 方法的目的是因为需要对post的body的值进行渲染
-        post =super().get_object(queryset=None)
-        md = markdown.Markdown(extensions=[
-            'markdown.extensions.extra',
-            'markdown.extensions.codehilite',
-            # 记得在顶部引入TocExtension 和 Slugify
-            TocExtension(slugify=slugify),
-            ])
-        post.body = md.convert(post.body)
-
-        m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
-        post.toc = m.group(1) if m is not None else ''
-
-        return post
-
-
-# def archive(request, year, month):
-#     post_list = Post.objects.filter(created_time__year=year,
-#                                     created_time__month=month
-#                                     ).order_by('-created_time')
-#     return render(request, 'web/index.html', context={'post_list': post_list})
-class ArchiveView(ListView):
-    model = Post
-    template_name = 'web/index.html'
-    context_object_name = 'post_list'
-
+class ArchiveView(IndexView):
     def get_queryset(self):
-        year = self.kwargs.get('year')
-        month = self.kwargs.get('month')
-        return super().get_queryset().filter(created_time__year=year,created_time__month=month)
+        year = self.kwargs.get("year")
+        month = self.kwargs.get("month")
+        return (
+            super()
+            .get_queryset()
+            .filter(created_time__year=year, created_time__month=month)
+        )
 
 
 # def category(request, pk):
@@ -96,44 +58,23 @@ class ArchiveView(ListView):
 #     return render(request, 'web/index.html', context={'post_list': post_list})
 
 class CategoryView(IndexView):
-
     def get_queryset(self):
-        cate = get_object_or_404(category, pk=self.kwargs.get('pk'))
-        return super(CategoryView,self).get_queryset().filter(category=cate)
+        cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
+        return super(CategoryView, self).get_queryset().filter(category=cate)
 
-# def tag(request, pk):
-#     t = get_object_or_404(Tag, pk=pk)
-#     post_list = Post.objects.filter(tags=t)
-#     return render(request, 'web/index.html', context={'post_list': post_list})
 
-class TagView(ListView):
-    model = Tag
-    template_name = 'web/index.html'
-    context_object_name = 'post_list'
-
+class TagView(IndexView):
     def get_queryset(self):
-        t = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
-        return super(TagView,self).get_queryset().filter(tag=t)
+        t = get_object_or_404(Tag, pk=self.kwargs.get("pk"))
+        return super().get_queryset().filter(tags=t)
 
-# Create your views here.
-
-# def search(request):
-#     q = request.GET.get('q')
-
-#     if not q:
-#         error_msg = "请输入搜索关键词"
-#         messages.add_message(request,messages.ERROR, error_msg, extra_tags='danger')
-#         return redirec('blog:index')
-
-#     post_list = Post.objects.filter(Q(title_icontains=q) | Q(body_icontains=q))
-#     return render(request, 'web/index.html', {'post_list':post_list})
 def search(request):
     q = request.GET.get('q')
- 
+
     if not q:
         error_msg = "请输入搜索关键词"
         messages.add_message(request, messages.ERROR, error_msg, extra_tags='danger')
-        return redirect('web:index')
- 
+        return render('web:index')
+
     post_list = Post.objects.filter(Q(title__icontains=q) | Q(body__icontains=q))
     return render(request, 'web/index.html', {'post_list': post_list})
